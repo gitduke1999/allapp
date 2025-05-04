0d09076c0202c8adb448ec42027ed963de95018c
GitDuke <rainer001@web.de>
2025-05-04 17:41:13 +0200

    Aktualisieren von Kostenpunkt.java

--- app/src/main/java/de/indix/allapp/Kostenpunkt.java
+++ app/src/main/java/de/indix/allapp/Kostenpunkt.java
@@ -1,5 +1,11 @@
 package de.indix.allapp;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverters;
+
+import java.util.List;
+
 @Entity
 public class Kostenpunkt {
     @PrimaryKey(autoGenerate = true)
