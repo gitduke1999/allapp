64bf566afb4c0ac39081a65ce659303987c85702
GitDuke <rainer001@web.de>
2025-05-04 17:29:39 +0200

    Kostenpunkt.java erstellen

---
+++ app/src/main/java/de/indix/allapp/Kostenpunkt.java
@@ -0,0 +1,13 @@
+@Entity
+public class Kostenpunkt {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String name;
+    public String kategorie;
+    public int abbuchungstag;
+    public double betrag;
+
+    @TypeConverters(MonatsConverter.class)
+    public List<Integer> abbuchungsmonate;
+}
