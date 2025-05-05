df8b2871535f7608577c514557d76aec76817cb5
GitDuke <rainer001@web.de>
2025-05-05 13:10:55 +0200

    Update SecondFragment.java

--- app/src/main/java/de/indix/allapp/SecondFragment.java
+++ app/src/main/java/de/indix/allapp/SecondFragment.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import de.indix.allapp.FragmentSecondBinding;
+
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
