80ae0db7da1a1af6fd6c70f9da04bb0e64f07565
GitDuke <rainer001@web.de>
2025-05-05 05:59:03 +0200

    Create ErfassungsmaskeActivity.java

---
+++ app/src/main/java/de/indix/allapp/ErfassungsmaskeActivity.java
@@ -0,0 +1,56 @@
+package de.indix.allapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.indix.allapp.viewmodel.KostenpunktViewModel;
+import de.indix.allapp.model.Kostenpunkt;
+
+
+public class ErfassungsmaskeActivity extends AppCompatActivity {
+    EditText name, kategorie, tag, betrag, monate;
+    Button speichern;
+    KostenpunktViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_erfassungsmaske);
+
+        name = findViewById(R.id.nameInput);
+        kategorie = findViewById(R.id.kategorieInput);
+        tag = findViewById(R.id.tagInput);
+        betrag = findViewById(R.id.betragInput);
+        monate = findViewById(R.id.monateInput);
+        speichern = findViewById(R.id.speichernButton);
+
+        viewModel = new ViewModelProvider(this).get(KostenpunktViewModel.class);
+
+        speichern.setOnClickListener(v -> {
+            Kostenpunkt k = new Kostenpunkt();
+            k.name = name.getText().toString();
+            k.kategorie = kategorie.getText().toString();
+            k.abbuchungstag = Integer.parseInt(tag.getText().toString());
+            k.betrag = Double.parseDouble(betrag.getText().toString());
+            k.abbuchungsmonate = parseMonate(monate.getText().toString());
+
+            viewModel.insert(k);
+            finish();
+        });
+    }
+
+    private List<Integer> parseMonate(String input) {
+        List<Integer> result = new ArrayList<>();
+        for (String s : input.split(",")) {
+            result.add(Integer.parseInt(s.trim()));
+        }
+        return result;
+    }
+}
