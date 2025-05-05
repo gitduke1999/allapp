52efc1e56a7fcbfb672472ccc381dbd72e6362e2
GitDuke <rainer001@web.de>
2025-05-05 05:54:31 +0200

    Create KostenpunktViewModel.java

---
+++ app/src/main/java/de/indix/allapp/KostenpunktViewModel.java
@@ -0,0 +1,58 @@
+package de.indix.allapp;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.indix.allapp.data.AppDatabase;
+import de.indix.allapp.data.KostenpunktDao;
+import de.indix.allapp.model.Kostenpunkt;
+
+
+public class KostenpunktViewModel extends AndroidViewModel {
+    private final KostenpunktDao dao;
+    private final LiveData<List<Kostenpunkt>> alleKostenpunkte;
+
+    public KostenpunktViewModel(@NonNull Application application) {
+        super(application);
+        dao = AppDatabase.getInstance(application).kostenpunktDao();
+        alleKostenpunkte = dao.getAll();
+    }
+
+    public LiveData<List<Kostenpunkt>> getAlleKostenpunkte() {
+        return alleKostenpunkte;
+    }
+
+    public void insert(Kostenpunkt k) {
+        dao.insert(k);
+    }
+
+    public void update(Kostenpunkt k) {
+        dao.update(k);
+    }
+
+    public void delete(Kostenpunkt k) {
+        dao.delete(k);
+    }
+
+    public Map<Integer, Double> berechneMonatskosten() {
+        List<Kostenpunkt> liste = dao.getAllSync();
+        Map<Integer, Double> monatsSummen = new HashMap<>();
+
+        for (int i = 1; i <= 12; i++) monatsSummen.put(i, 0.0);
+
+        for (Kostenpunkt k : liste) {
+            for (int monat : k.abbuchungsmonate) {
+                monatsSummen.put(monat, monatsSummen.get(monat) + k.betrag);
+            }
+        }
+
+        return monatsSummen;
+    }
+}
